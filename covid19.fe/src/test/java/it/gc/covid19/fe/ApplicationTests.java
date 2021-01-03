20a87b559d516d92b37374ade14a0b614185ccd1
Giuseppe Cinque <gfcinque@inwind.it>
2021-01-03 14:30:05 +0100

    progetto fe per la visualizzazione del grafico

---
+++ covid19.fe/src/test/java/it/gc/covid19/fe/ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gc.covid19.fe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
